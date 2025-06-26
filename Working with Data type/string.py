c42361d1beb2c23355644f18d650df0a13f684dd
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-06-26 09:28:01 +0600

    2nd commit

---
+++ Working with Data type/string.py
@@ -0,0 +1,12 @@
+# Python is Dynamically typed language
+a = 'this is a string'
+b= "this is also a string"
+c="""this is also also string"""
+
+print(a)
+print(b)
+print(c)
+
+print (type(a))
+print (type(b))
+print (type(c))
