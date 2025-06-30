c8a7debad0bd29ae588e55f8d317db19110dc424
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-07-01 01:39:06 +0600

    3rd commit

---
+++ Working with Data type/strng.py
@@ -0,0 +1,5 @@
+a = "this is a string"
+print(len(a)) #lenght of string
+print(a[6])
+print(a[len(a)-1])
+print(a[-1])
--- first_code.py
+++ first_code.py
@@ -1,1 +1,14 @@
+x = 2
+y = 3
+sum = x + y
+sub = x - y
+mul = x * y
+div = x/y
+print(sum)
+print(sub)
+print(mul)
+print(div)
+
+
+
 print("hello world")
