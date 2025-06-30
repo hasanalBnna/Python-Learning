acd39dde1d34c7983cd288a545f8970703dc03ab
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-07-01 03:10:38 +0600

    4th commit

---
+++ Working with Data type/string_methods.py
@@ -0,0 +1,12 @@
+a = "rahim"
+print(a.title())
+b = a.title()
+print(b)
+print(a.upper())
+print(a.lower())
+print(a.swapcase())
+
+#replace
+x = "i eat rice"
+y = x.replace("rice","Mango")
+print(y)
--- Working with Data type/strng.py
+++ Working with Data type/strng.py
@@ -3,3 +3,9 @@ print(len(a)) #lenght of string
 print(a[6])
 print(a[len(a)-1])
 print(a[-1])
+
+#immutable data type
+b = "Karim"
+print(b[1])
+b[1] = 'o'
+print(b)
