3f22f87bf0af74f49f7e2cf9dcf62d4a72adcf1e
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-07-04 01:05:41 +0600

    5th commit

---
+++ Working with Data type/string_format.py
@@ -0,0 +1,18 @@
+a = "Good morning, Karim. How are you?"
+
+user_input = input("Whats your Name?\n")
+b = "Good morning {}, How are you? \ni am {}".format(user_input,"fine")
+print(user_input)
+print(b)
+c = "i am also {}".format("fine")
+print(c)
+print("Can you tell me whats your first name?")
+F_name = input("Yes,my fname is : ")
+print("Can you tell me whats your last name?")
+L_name = input("Yes,my lname is : ")
+print("may i know your age, please?")
+age = input("Yes,my age is : ")
+#user_input =input("What is your name?")
+d ="my name is {} {} \nand i am {} years old".format(F_name,L_name,age)
+print(d)
+
