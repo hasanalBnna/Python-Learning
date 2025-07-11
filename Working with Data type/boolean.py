eb523aea6cebcf6f65a24a36d215f2d0900cdd02
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-07-11 12:57:45 +0600

    uncounted commit

---
+++ Working with Data type/boolean.py
@@ -0,0 +1,21 @@
+# boolean data type
+MyName =True
+MyName =False
+print(MyName)
+
+#none data type
+
+taka = None
+print(taka)
+
+#mapping(dicsionary data type)
+
+person={
+    'first_name':'kazi',
+    'last_name':'Banna',
+    'age':26,
+    'isBangladeshi':True
+}
+
+print(person['first_name'])
+print(person)
