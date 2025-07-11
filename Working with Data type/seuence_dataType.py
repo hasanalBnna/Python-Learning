730dc4cb30b0116c8082ade911948cff08175c31
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-07-11 12:19:00 +0600

    uncounted commit

---
+++ Working with Data type/seuence_dataType.py
@@ -0,0 +1,12 @@
+# list data type
+latter = ["A","B",2,3.33,"A",3.33]
+print(latter[3])
+
+#tuple data type
+alpha = ("a","b","a",2,2.22,2)
+print(alpha)
+
+#range data type
+data = range(0,11,2) #(start, end + 1,difference)
+data2 = range(11) # it also return 0-10
+print(*data2)
