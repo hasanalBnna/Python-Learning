71dd4a66363733401dc273d5f4d8aa9437e85f14
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-07-12 13:28:05 +0600

    uncounted commit

--- Working with Data type/boolean.py
+++ Working with Data type/boolean.py
@@ -19,3 +19,14 @@ person={
 
 print(person['first_name'])
 print(person)
+
+#set deta type -mutable
+myset = {"me","my","he","my","me",2,3,5,3,2} #avoid duplicate value
+print(myset)
+
+
+
+#frozen data type- immutable
+
+myfrize =frozenset({"me","my","he","my","me",2,3,5,3,2})
+print(myfrize)
---
+++ Working with Data type/chk_dataType.py
@@ -0,0 +1,26 @@
+
+a = 10
+b = 3.14
+c = 1 + 2j
+d = "Hello"
+e = ['apple','banana','cherry']
+f = (1, 2, 3)
+g = range(1, 10, 2)
+h = {'name': 'Alice', 'age': 25}
+i = {1, 2, 3}
+j = frozenset([1, 2, 2, 3])
+k = True
+l = None
+
+print("Output of a", type(a))
+print("Output of b", type(b))
+print("Output of c", type(c))
+print("Output of d", type(d))
+print("Output of e", type(e))
+print("Output of f", type(f))
+print("Output of g", type(g))
+print("Output of h", type(h))
+print("Output of i", type(i))
+print("Output of j", type(j))
+print("Output of k", type(k))
+print("Output of l", type(l))
