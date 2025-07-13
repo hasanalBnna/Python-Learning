1726c2e0ed8c17cf06ba8787c0fa5d229c7d0d0f
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-07-14 01:29:42 +0600

    uncounted commit

---
+++ Working with Data type/console_project.py
@@ -0,0 +1,34 @@
+#1st project
+# name = input("Enter your name: ")
+# print("Hello",name,"|")
+
+#2nd project
+# num1 = input("Enter 1st number: ")
+# num2 = input("Enter 2nd number: ")
+
+# num1 = int(num1)
+# num2 = int(num2)
+
+# sum = num1 + num2
+# print("sum of num1 and num2 =",sum)
+
+#3rd project
+# celsius = input("Enter Celsius: ")
+# celsius = float(celsius)
+
+# fahrenheit = (celsius* 9/5) + 32
+
+# print(fahrenheit)
+
+#4th project
+principal = input("Enter the principal amount: ")
+rate = input("Enter the rate of interest: ")
+time = input("Enter the time (In year): ")
+
+principal = float(principal)
+rate = float(rate)
+time = float(time)
+
+interest = (principal*rate*time)/100
+
+print(interest)
