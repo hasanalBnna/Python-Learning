64a247dae95d5adfc3fd6ab1c2e650d9ee6e0a1b
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-07-17 09:37:29 +0600

    uncounted commit

---
+++ Working with Data type/String_concate+repetition.py
@@ -0,0 +1,20 @@
+# string repetition
+name = "Banna "
+repeat = name * 5
+print(repeat)
+
+#string concatenation:
+str1 = "Hello"
+str2 = "World"
+
+# combine = str1+",""?"+str2+"."
+
+#join()
+#combine =",".join([str1,str2])+ "?" #Hello,World?
+
+#format()
+# combine = "{} {} ?".format(str1,str2) #Hello World ?
+
+#modulas %
+combine = "%s %s ?" %(str1,str2) #Hello World ?
+print(combine)
