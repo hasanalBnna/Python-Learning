fa02fd0850edfdf3669318780e51359da60a6c62
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-07-17 08:31:05 +0600

    uncounted commit

---
+++ Working with Data type/String_slice.py
@@ -0,0 +1,13 @@
+text = "Hello World"
+
+#start String slicing
+print(text[0:7]) #Hello W
+print(text[6:7]) #w
+print(text[:7]) #Hello W
+
+#stop string slicing
+print(text[7:]) #orld
+
+
+#step string slicing
+print(text[0::2]) #Hlowrd skip few step
