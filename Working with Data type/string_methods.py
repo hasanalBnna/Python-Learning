a196ba3d18cc52f72f258d156bb7390b7e4b3bed
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-07-19 00:06:19 +0600

    uncounted commit

--- Working with Data type/string_methods.py
+++ Working with Data type/string_methods.py
@@ -1,12 +1,34 @@
-a = "rahim"
-print(a.title())
-b = a.title()
-print(b)
-print(a.upper())
-print(a.lower())
-print(a.swapcase())
+# a = "rahim ahmed"
+# #title
+# print(a.title())
+# b = a.title()
+# print(b)
+# #upper
+# print(a.upper())
+# #lower
+# print(a.lower())
+# #swap
+# print(a.swapcase())
 
-#replace
-x = "i eat rice"
-y = x.replace("rice","Mango")
-print(y)
+# #replace
+# x = "i eat rice"
+# y = x.replace("rice","Mango")
+# print(y)
+
+#split the string into a list
+text = "Hello-World- this-is-Python"
+print(text.split("-"))
+
+# nums = [10, 20, 30, 40, 50]
+
+# print(nums[ : :-2 ]) #nums[start:stop:step]
+
+#join list into a string
+txt = ['Hello', 'World', ' this', 'is', 'Python']
+print(" ".join(txt))
+
+#strip to remove white space
+txt2 = "  hello world "
+print(txt2.strip())
+
+print(txt2.rstrip())
