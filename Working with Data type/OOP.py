6d5c94be345c459a935474409d463e4b985ca774
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-07-23 23:57:55 +0600

    uncounted commit

---
+++ Working with Data type/OOP.py
@@ -0,0 +1,17 @@
+class Car:
+    def __init__(self):
+
+        self.brand = ""
+        self.model = ""
+
+c1 = Car()
+c1.brand = "toyota"
+c1.model = "civic"
+print(c1.brand)  
+print(c1.model) 
+
+c2 = Car()
+c2.brand = "honda"
+c2.model = "corolla"
+print(c2.brand)  
+print(c2.model) 
