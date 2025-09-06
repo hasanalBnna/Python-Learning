031685a2196a6a7f6b42697292e196d899b7b465
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2025-09-06 16:47:41 +0600

    uncounted commit

---
+++ Module-10/DSA/dsa-1.py
@@ -0,0 +1,53 @@
+# x = ["abc", 34, True, 40, "male"]
+# for i in x:
+#     print (i)
+
+# x  =["abc", 34, True, 40, "male"]
+# x.append("Banna")
+# print (x)
+
+# x  =["abc", 34, True, 40, "male"]
+# x.insert(2,"Banna")
+# print (x)
+
+# x  =["abc", 34, True, 40, "male"]
+# x.remove(40)
+# print (x)
+
+# x  =["abc", 34, True, 40, "male"]
+# print(x[2:4])
+
+# x  =["abc", 34, True, 40, "male"]
+# print(x[:4])
+
+# x  =["abc", 34, "Banna", True, 40, "male"]
+# if "banna" in x:
+#     print("Yes, 'Banna' is in the list")
+# else:
+#     print("No, 'Banna' is not in the list")
+
+
+# x = ["abc", 34, "Banna", True, 40, "male"]
+# if any(isinstance(item, str) and item.lower() == "baNna" for item in x):
+#     print("Yes, 'Banna' is in the list")
+# else:
+#     print("No, 'Banna' is not in the list")
+
+
+
+# x = ["Abc", 34, "Banna", True, 40, "male"]
+# input_str=input("Enter the string to search: ")
+# if any(isinstance(item, str) and item.lower() == input_str for item in x):
+#     print(f"Yes, '{input_str}' is in the list")
+# else:
+#     print(f"No, '{input_str}' is not in the list")
+
+# x = ["Abc", 34, "Banna", True, 40, "male"]
+# x[1]= "Banana"
+# print (x)
+
+x = ["Abc", 34, "Banna", True, 40, "male"]
+y= [1,2,3,4,5, "Banana"]
+x.extend(y)
+print (x)
+
